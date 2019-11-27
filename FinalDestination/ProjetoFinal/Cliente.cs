6498ba44ca720e6cead88ad80756f8b7fdf166ec
Gilton Sampaio de Souza Júnior <gilton.j@escolar.ifrn.edu.br>
2019-11-27 15:12:03 -0300

    christian

---
+++ FinalDestination/ProjetoFinal/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFinal
+{
+    class Cliente
+    {
+        private string nome;
+        private string fone;
+        private string email;
+        private string resumoProjeto;
+        private string pagamento;
+
+        struct Clientes
+        {
+            public string nome;
+            public string fone;
+            public string email;
+            public string resumoProjeto;
+            public string pagamento;
+        }
+
+    }
+}
---
+++ Triangulo/Triangulo/DisciplinaSemestral.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    class DisciplinaSemestral
+    {
+        private double n1;
+        private double n2;
+      
+        
+        public DisciplinaSemestral(double n1, double n2)
+        {
+              this.n1 = n1;
+              this.n2 = n2;
+        }
+
+        public double GetMedia()
+        {
+            return (n1 + n2) / 2;
+        }
+    }
+}
